ee2afee15da834192a947662c515196f013dbd8b
Svetlozar Stafanov <svetlozarstefanov@hotmail.com>
2017-09-28 22:12:43 +0300

    Added third task for second homework for BootCamp

---
+++ Homework2/Task3/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Program
+    {
+        public static string ReverseString(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+            return s.Last<char>().ToString() + ReverseString(s.Substring(0,s.Length-1));  
+       }
+        public static void Main(string[] args)
+        {
+            string s = "abc";
+            Console.WriteLine(ReverseString(s));
+        }
+    }
+}
