2cc454703534e3e12d3b23fac0fe118cc3d3b4a6
Svetlozar Stafanov <svetlozarstefanov@hotmail.com>
2017-09-28 21:38:10 +0300

    Added second task for second homework for BootCamp

---
+++ Homework2/Task2/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Program
+    {
+        public static void FindIndex(int[] array)
+        {
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int i = 1; i < array.Length - 1; i++)
+            {
+                for(int j = 0;j < i; j++)
+                {
+                    sum1 += array[j];
+                }
+                for(int k = i+1; k < array.Length; k++)
+                {
+                    sum2 += array[k];
+                }
+                if(sum1 == sum2)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    sum1 = 0;
+                    sum2 = 0;
+                }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int[] array = { 1,2,3,4,6};
+            FindIndex(array);
+        }
+    }
+}
